9853265d58eb6ba30f7d7ee56595225701fd50d4
bcore7777 <britney.core7@gmail.com>
2016-04-17 22:00:15 -0400

    Added Calendar, Styled Calendar & Added A Link To Return Screen
    Also added material ng to use calendar material with angular

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -102,7 +102,7 @@ MyFol.controller('ClasslistCtrl', ['$scope', '$http', function($scope, $http){
 	});
 }]);
 
-MyFol.controller('CalendarCtrl', ['$scope', '$http', function($scope, $http){
+MyFol.controller('CalendarCtrl', ['$scope', '$filter', '$http', '$q', function($scope, $filter, $http, $q){
     $scope.CalendarCourse = false;
 
 	$scope.ShowHide3 = function () {
@@ -119,7 +119,54 @@ MyFol.controller('CalendarCtrl', ['$scope', '$http', function($scope, $http){
 	});
 
     //Calendar scope
-	$scope.day = moment();
+	//$scope.day = moment();
+	
+	$scope.dayFormat = "d";
+
+  // To select a single date, make sure the ngModel is not an array.
+  $scope.selectedDate = null;
+
+  // If you want multi-date select, initialize it as an array.
+  $scope.selectedDate = [];
+
+  $scope.firstDayOfWeek = 0; // First day of the week, 0 for Sunday, 1 for Monday, etc.
+  $scope.setDirection = function(direction) {
+    $scope.direction = direction;
+    $scope.dayFormat = direction === "vertical" ? "EEEE, MMMM d" : "d";
+  };
+
+  $scope.dayClick = function(date) {
+    $scope.msg = "You clicked " + $filter("date")(date, "MMM d, y h:mm:ss a Z");
+  };
+
+  $scope.prevMonth = function(data) {
+    $scope.msg = "You clicked (prev) month " + data.month + ", " + data.year;
+  };
+
+  $scope.nextMonth = function(data) {
+    $scope.msg = "You clicked (next) month " + data.month + ", " + data.year;
+  };
+
+  $scope.tooltips = true;
+  $scope.setDayContent = function(date) {
+
+  // You would inject any HTML you wanted for
+  // that particular date here.
+        return "<p>jkjhgty</p>";
+
+        // You could also use an $http function directly.
+       // return $http.get("/some/external/api");
+      // return $http.get('../index.php/user/allCalendar');
+
+        // You could also use a promise.
+       // var deferred = $q.defer();
+        //$timeout(function() {
+          //  deferred.resolve("<p>jhg</p>");
+        //}, 1000);
+        // return deferred.promise;
+
+    };
+	
 }]);
 
 MyFol.controller('SettingsCtrl', ['$scope', '$http', function($scope, $http){
